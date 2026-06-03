571d688f714e508aab7f59e982a4c756fd5c58fb
Ogunnaike Funto <ogunnaikef1@gmail.com>
2026-06-03 11:53:29 +0100

    Add missing Terraform outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,23 +1,29 @@
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+output "alb_controller_role_arn" {
+  value = module.iam.alb_controller_role_arn
 }
 
-output "cluster_name" {
-  value = module.eks.cluster_name
+output "carts_dynamodb_role_arn" {
+  value = module.iam.carts_dynamodb_role_arn
 }
 
-output "region" {
-  value = var.aws_region
+output "mysql_host" {
+  value = module.rds.mysql_host
 }
 
-output "vpc_id" {
-  value = module.vpc.vpc_id
+output "postgres_host" {
+  value = module.rds.postgres_host
 }
 
-output "assets_bucket_name" {
-  value = "bedrock-assets-alt-soe-025-4736"
+output "dev_access_key_id" {
+  value = module.iam.dev_access_key_id
 }
 
-output "alb_url" {
-  value = "Check kubectl get ingress -n retail-app for ALB URL"
+output "dev_secret_access_key" {
+  value     = module.iam.dev_secret_access_key
+  sensitive = true
+}
+
+output "dev_console_password" {
+  value     = module.iam.dev_console_password
+  sensitive = true
 }
